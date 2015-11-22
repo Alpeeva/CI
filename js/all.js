bf2700493c2a7ef3f832e89db6a112002461b9a4
Kateryna Alpieieva <kalpieieva@gmail.com>
2015-11-23 01:14:48 +0200

    Update all.js
    Added multiply function

--- js/all.js
+++ js/all.js
@@ -5,3 +5,11 @@ var summarize = function (a) {
 };
 
 console.log(summarize(5)(3));
+
+var multiply = function (a) {
+    return function (b) {
+        return a * b;
+    };
+};
+
+console.log(multiply(5)(3));
